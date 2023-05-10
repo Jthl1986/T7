dd336f37bd22b7fbdc045231d22b0d795fe6c0c4
Santacecilia <jorgesantacecilia@gmail.com>
2023-05-10 16:23:25 -0300

    9

--- app3.py
+++ app3.py
@@ -757,7 +757,6 @@ with st.sidebar:
     url = "https://raw.githubusercontent.com/Jthl1986/T7/master/—Pngtree—pin map leaf sprout agriculture_3554514.png"
     st.markdown(f'<div style="margin-top: -150px;"><img src="{url}" style="object-fit: cover; width: 100%; height: 100%;"></div>', unsafe_allow_html=True)
     st.markdown('<h1 style="margin-top: -100px;">AgroApp</h1>', unsafe_allow_html=True)
-    st.markdown('<h1 style="margin-top: -100px;">---</h1>', unsafe_allow_html=True)
 my_button = st.sidebar.radio("Modulos",('Planteo productivo', 'Condiciones climáticas', 'Tenencia granos', 'Tenencia hacienda', 'Servicios agrícolas', 'Cuadro resumen'))
 if my_button == 'Tenencia hacienda':
     app()
