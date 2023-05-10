8bc4cb6d462226a7e9b1ce0d6e7647281b1ebb0e
Santacecilia <jorgesantacecilia@gmail.com>
2023-05-10 15:14:09 -0300

    9

--- app3.py
+++ app3.py
@@ -777,7 +777,6 @@ with st.sidebar:
     rss_url = "https://bichosdecampo.com/feed/"
     feed = feedparser.parse(rss_url)
     st.title("Noticias")
-    
     with marquee.marquee():
         for item in feed["items"][:5]:
             st.markdown(f"""<a href="{item['link']}" target="_blank">{item['title']}</a>""")
