bdf6c8e53644cf6930c78bc02a69eefb2a3d6281
Santacecilia <jorgesantacecilia@gmail.com>
2023-05-10 15:38:17 -0300

    9

--- app3.py
+++ app3.py
@@ -754,7 +754,7 @@ def app5():
 #configuraciones de página   
 lottie_book = load_lottieurl('https://assets7.lottiefiles.com/packages/lf20_d7OjnJ.json')
 with st.sidebar:
-    url = "https://raw.githubusercontent.com/Jthl1986/T7/blob/master/kisspng-sustainable-agriculture-sustainability-computer-ic-agriculture-5abc4783cb0219.9797877115222885158315.png"
+    url = "https://raw.githubusercontent.com/Jthl1986/T7/blob/master/pngwing.com.png"
     st.image(url, width=300)
     st.title('Agro App')
     st.markdown("---")
