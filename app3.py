c553ee0d00a7410faf9d10680c46148b5d28340e
Santacecilia <jorgesantacecilia@gmail.com>
2023-05-10 16:59:49 -0300

    9

--- app3.py
+++ app3.py
@@ -449,7 +449,7 @@ def app4():
     
 
 def app5():
-    st.markdown('<h4 style="margin-top: -60px; text-align: center;">Hoja Agro</h1>', unsafe_allow_html=True)
+    st.markdown('<h4 style="margin-top: -60px; text-align: center;">Cuadro resumen Actividad Agropecuaria</h1>', unsafe_allow_html=True)
     left, right = st.columns(2)
     css()
    
